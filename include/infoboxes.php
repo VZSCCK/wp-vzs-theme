62b89f41e0960a7fccb0027dfb6b1c1731bac6d3
behounek <tomas@behounek.cz>
2017-02-02 12:41:35 +0100

    Update infoboxes.php
    Přidán 3box

--- include/infoboxes.php
+++ include/infoboxes.php
@@ -1,4 +1,10 @@
-<div class="infobox">
+            <div class="infobox">
+              <h2 class="title">Služba a IZS</h2>
+              <?php the_field('text_box3'); ?>
+              <div class="text-center infobox-button"><a href="<?php echo get_permalink(12); ?>" class="button">Více informací <i class="fa fa-angle-right"></i></a></div>
+            </div>
+
+            <div class="infobox">
               <h2 class="title">Vzdělávání a výcvik</h2>
               <?php the_field('text_box1'); ?>
               <div class="text-center infobox-button"><a href="<?php echo get_permalink(14); ?>" class="button">Více informací <i class="fa fa-angle-right"></i></a></div>
