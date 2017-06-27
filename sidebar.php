69b87428aa5ad6fc9e5eb1d09118eeb828b8751b
behounek <tomas@behounek.cz>
2017-06-27 10:18:10 +0200

    GIVT přidání loga

--- sidebar.php
+++ sidebar.php
@@ -28,6 +28,8 @@
 
           <a target="_blank" href="http://darcovskasms.cz/projekt-957/bezpeCnE-u-vody-pomozte-nAm-pomAhat.html" class="banner"><img src="<?php echo BASE_URL; ?>/images/banner.jpg" alt="Dárcovská DMS"></a>
 
+          <a target="_blank" href="http://bit.ly/Podporuji-Vodní-Záchrannou-Službu" class="banner"><img src="<?php echo BASE_URL; ?>/images/banner2.jpg" alt="GIVT - klikni a pomáhej"></a>
+
           <div id="calendar">
             <iframe src="https://www.google.com/calendar/hosted/vzs.cz/embed?showTitle=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=vzs.cz_r7fgkv0ev9trmgen3r0ic1vul4%40group.calendar.google.com&amp;color=%2342104A&amp;src=host%40vzs.cz&amp;color=%23BE6D00&amp;src=info%40vzs.cz&amp;color=%232F6309&amp;src=cs.czech%23holiday%40group.v.calendar.google.com&amp;color=%FFFFFF23&amp;ctz=Europe%2FPrague" height="300" style="border: 1px solid #eee;"></iframe>
             <div class="text-center"><a href="<?php echo get_permalink(39); ?>">Zobrazit celý kalendář</a></div>
