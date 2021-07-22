ebcf34700a675850de94592e732c6b382a5908d1
behounek <tomas@behounek.cz>
2021-07-22 21:36:54 +0200

    Uprava menu na strance prevence

--- sidebar.php
+++ sidebar.php
@@ -25,14 +25,17 @@
             <div class="text-center"><a href="<?php echo get_category_link(1); ?>" class="all">Všechny aktuality</a></div>
             <div class="fcb text-center"><a href="https://www.facebook.com/VZSCCK" target="_blank" class="fcb-link"><img src="<?php echo BASE_URL; ?>/images/fcb.png" alt="Facebook VSZ"></a></div>
           </div>
-<!-- 
-          <a target="_blank" href="https://www.zachrankaapp.cz" class="banner"><img src="<?php echo BASE_URL; ?>/images/banner3.jpg" alt="Aplikace ZACHRANKA"></a>
-          
-          <a target="_blank" href="http://bit.ly/Podporuji-Vodní-Záchrannou-Službu" class="banner"><img src="<?php echo BASE_URL; ?>/images/banner2.jpg" alt="GIVT - klikni a pomáhej"></a>
 
-          <div id="calendar">
-                    <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;ctz=Europe%2FPrague&amp;src=dnpzLmN6X3I3ZmdrdjBldjl0cm1nZW4zcjBpYzF2dWw0QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%2342104A&amp;src=aG9zdEB2enMuY3o&amp;color=%23BE6D00&amp;src=aW5mb0B2enMuY3o&amp;color=%232F6309&amp;src=Y3MuY3plY2gjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%FFFFFF23&amp;" height="300" style="border: 1px solid #eee;"></iframe>   
-                    <div class="text-center"><a href="<?php echo get_permalink(39); ?>">Zobrazit celý kalendář</a></div>
-          </div>
--->
+<?php
+          if($post->ID == 18 || ($post->post_parent == 18) ;
+             else { ?>
+                    <a target="_blank" href="https://www.zachrankaapp.cz" class="banner"><img src="<?php echo BASE_URL; ?>/images/banner3.jpg" alt="Aplikace ZACHRANKA"></a>
+          
+                    <a target="_blank" href="http://bit.ly/Podporuji-Vodní-Záchrannou-Službu" class="banner"><img src="<?php echo BASE_URL; ?>/images/banner2.jpg" alt="GIVT - klikni a pomáhej"></a>
+
+                    <div id="calendar">
+                              <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;ctz=Europe%2FPrague&amp;src=dnpzLmN6X3I3ZmdrdjBldjl0cm1nZW4zcjBpYzF2dWw0QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%2342104A&amp;src=aG9zdEB2enMuY3o&amp;color=%23BE6D00&amp;src=aW5mb0B2enMuY3o&amp;color=%232F6309&amp;src=Y3MuY3plY2gjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%FFFFFF23&amp;" height="300" style="border: 1px solid #eee;"></iframe>   
+                              <div class="text-center"><a href="<?php echo get_permalink(39); ?>">Zobrazit celý kalendář</a></div>
+                    </div>
+          <?php } ?> 
         </div>
